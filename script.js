29f72b999515ed1068493977b3e99f3a38888596
xylo <141632751+xylolo@users.noreply.github.com>
2024-02-07 17:14:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const upload = multer({ storage: storage });
 
 // Serve the HTML form
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/test.html');
 });
 
 // Handle file upload
